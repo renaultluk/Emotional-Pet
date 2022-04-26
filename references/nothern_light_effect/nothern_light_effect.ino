df38b38ce94360e37535f2e8f5cbabf56f44570b
CarsonChanHKUST <99250138+CarsonChanHKUST@users.noreply.github.com>
2022-04-26 22:56:29 +0800

    changed from northern light to generic led effect.
    Issue found: multiple functions called within a loop would make the color randomly assign onto the led strip instead of normally overwrite the previous led colors.

--- references/nothern_light_effect/nothern_light_effect.ino
+++ references/nothern_light_effect/nothern_light_effect.ino
@@ -1,5 +1,17 @@
-// NeoPixel test program showing use of the WHITE channel for RGBW
-// pixels only (won't look correct on regular RGB NeoPixel strips).
+// A basic everyday NeoPixel strip test program.
+
+// NEOPIXEL BEST PRACTICES for most reliable operation:
+// - Add 1000 uF CAPACITOR between NeoPixel strip's + and - connections.
+// - MINIMIZE WIRING LENGTH between microcontroller board and first pixel.
+// - NeoPixel strip's DATA-IN should pass through a 300-500 OHM RESISTOR.
+// - AVOID connecting NeoPixels on a LIVE CIRCUIT. If you must, ALWAYS
+//   connect GROUND (-) first, then +, then data.
+// - When using a 3.3V microcontroller with a 5V-powered NeoPixel strip,
+//   a LOGIC-LEVEL CONVERTER on the data line is STRONGLY RECOMMENDED.
+// (Skipping these may work OK on your workbench but can fail in the field)
+unsigned long startMillis;
+unsigned long currentMillis;
+int i = 0;
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
@@ -8,13 +20,10 @@
 
 // Which pin on the Arduino is connected to the NeoPixels?
 // On a Trinket or Gemma we suggest changing this to 1:
-#define LED_PIN     6
+#define LED_PIN    6
 
 // How many NeoPixels are attached to the Arduino?
-#define LED_COUNT  8
-
-// NeoPixel brightness, 0 (min) to 255 (max)
-//#define BRIGHTNESS 50 // Set BRIGHTNESS to about 1/5 (max = 255)
+#define LED_COUNT 10
 
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_RGB + NEO_KHZ800);
@@ -27,14 +36,10 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_RGB + NEO_KHZ800);
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 
-int fadeAmount = 5;    // how many points to fade the LED by
-int LED_Brightness[LED_COUNT];
+
+// setup() function -- runs once at startup --------------------------------
 
 void setup() {
-  int LED_Brightness[LED_COUNT] = {};
-  for(int i=0; i<LED_COUNT; i++) {
-    LED_Brightness[i]= 50+ (rand() % 155);
-    }
   // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
   // Any other board, you can remove this part (but no harm leaving it):
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
@@ -44,34 +49,108 @@ void setup() {
 
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
-
-  for(int i=0; i<LED_COUNT; i++) {
-    LED_Brightness[i]= 100+ (rand() % 155);
-    }
+  strip.setBrightness(25); // Set BRIGHTNESS to about 1/5 (max = 255)
+  startMillis = millis();
+  Serial.begin(115200);
 }
+
+
+// loop() function -- runs repeatedly as long as board is on ---------------
 
 void loop() {
+  currentMillis = millis();
+  if (i > LED_COUNT) {
+      i = 0;
+    }
+  Serial.println(currentMillis);
   // Fill along the length of the strip in various colors...
-  colorWipe(strip.Color(0,   255,   0)     , 50); // Red
+  colorWipe(strip.Color(255,   0,   0), 50); // Red
+//  colorWipe(strip.Color(  0, 255,   0), 50); // Green
+//  colorWipe(strip.Color(  0,   0, 255), 50); // Blue
+
+  // Do a theater marquee effect in various colors...
+//  theaterChase(strip.Color(127, 127, 127), 50); // White, half brightness
+//  theaterChase(strip.Color(127,   0,   0), 50); // Red, half brightness
+//  theaterChase(strip.Color(  0,   0, 127), 50); // Blue, half brightness
+
+//  rainbow(10);             // Flowing rainbow cycle along the whole strip
+//  theaterChaseRainbow(50); // Rainbow-enhanced theaterChase variant
 }
+
+
+// Some functions of our own for creating animated effects -----------------
 
 // Fill strip pixels one after another with a color. Strip is NOT cleared
 // first; anything there will be covered pixel by pixel. Pass in color
 // (as a single 'packed' 32-bit value, which you can get by calling
 // strip.Color(red, green, blue) as shown in the loop() function above),
 // and a delay time (in milliseconds) between pixels.
-
 void colorWipe(uint32_t color, int wait) {
-
-  
-  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-    strip.setPixelColor(i, strip.Color(LED_Brightness[i], 0, 0));         //  Set pixel's color (in RAM)
-    strip.show();                          //  Update strip to match
-    
-    LED_Brightness[i] = LED_Brightness[i] + fadeAmount;
-    if (LED_Brightness[i] <= 50 || LED_Brightness[i] >= 255) {
-    fadeAmount = -fadeAmount;
+  Serial.print(i);
+  currentMillis = millis();
+  if (currentMillis - startMillis >= wait){
+    strip.setPixelColor(i, color);     //  Set pixel's color (in RAM)
+    strip.show();                      //  Update strip to match
+    i++;
+    startMillis = currentMillis;
   }
-    delay(10);                           //  Pause for a moment
+}
+
+// Theater-marquee-style chasing lights. Pass in a color (32-bit value,
+// a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
+// between frames.
+void theaterChase(uint32_t color, int wait) {
+  for(int a=0; a<10; a++) {  // Repeat 10 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in steps of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show(); // Update strip with new contents
+      delay(wait);  // Pause for a moment
+    }
+  }
+}
+
+// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+void rainbow(int wait) {
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this loop:
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    // strip.rainbow() can take a single argument (first pixel hue) or
+    // optionally a few extras: number of rainbow repetitions (default 1),
+    // saturation and value (brightness) (both 0-255, similar to the
+    // ColorHSV() function, default 255), and a true/false flag for whether
+    // to apply gamma correction to provide 'truer' colors (default true).
+    strip.rainbow(firstPixelHue);
+    // Above line is equivalent to:
+    // strip.rainbow(firstPixelHue, 1, 255, 255, true);
+    strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+  }
+}
+
+// Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
+void theaterChaseRainbow(int wait) {
+  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
+  for(int a=0; a<30; a++) {  // Repeat 30 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in increments of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        // hue of pixel 'c' is offset by an amount to make one full
+        // revolution of the color wheel (range 65536) along the length
+        // of the strip (strip.numPixels() steps):
+        int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
+        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show();                // Update strip with new contents
+      delay(wait);                 // Pause for a moment
+      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
+    }
   }
 }
