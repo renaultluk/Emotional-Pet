e6b8e1f19a816910e880001072b5fa82f2dd530d
Duallife <85776119+Duallife@users.noreply.github.com>
2022-05-19 20:07:08 +0800

    sample rate change

--- I2S.h
+++ I2S.h
@@ -3,7 +3,7 @@
 #include "freertos/task.h"
 #include "driver/i2s.h"
 #include "esp_system.h"
-#define SAMPLE_RATE (44100)
+#define SAMPLE_RATE (96000)
 #define PIN_I2S_BCLK 26
 #define PIN_I2S_LRC 22
 #define PIN_I2S_DIN 34
